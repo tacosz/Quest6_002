ecf7fdc84041b1aa1ca46105e723ec042beb72c4
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-21 15:16:09 +0700

    menambahkan data tetap untuk Data Jenis kelamin

--- app/src/main/java/com/example/myarsitekturmvvm/model/DataJK.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/model/DataJK.kt
@@ -1,4 +1,10 @@
 package com.example.myarsitekturmvvm.model
 
+import com.example.myarsitekturmvvm.R
+
 object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
 }
